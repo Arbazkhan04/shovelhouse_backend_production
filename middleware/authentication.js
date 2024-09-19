e7154ccf75bd9349e11c514e4ae5e53f6ae74ea7
Arbaz <arbaz582021@gmail.com>
2024-09-19 11:34:30 -0400

    rbac added sucessfully

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -1,22 +1,34 @@
-const User = require('../models/User')
-const jwt = require('jsonwebtoken')
-const { UnauthenticatedError } = require('../errors')
+const jwt = require('jsonwebtoken');
+const { UnauthenticatedError } = require('../errors');
 
 const auth = async (req, res, next) => {
-  // check header
-  const authHeader = req.headers.authorization
+  const authHeader = req.headers.authorization;
   if (!authHeader || !authHeader.startsWith('Bearer')) {
-    throw new UnauthenticatedError('Authentication invalid')
+    throw new UnauthenticatedError('Authentication invalid');
   }
-  const token = authHeader.split(' ')[1]
+  const token = authHeader.split(' ')[1];
 
   try {
-    const payload = jwt.verify(token, process.env.JWT_SECRET)
-    req.user = { userId: payload.userId, name: payload.name }
-    next()
+    const payload = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = {
+      userId: payload.userId,
+      name: payload.name,
+      role: payload.role,
+    };
+    next();
   } catch (error) {
-    throw new UnauthenticatedError('Authentication invalid')
+    throw new UnauthenticatedError('Authentication invalid');
   }
-}
+};
 
-module.exports = auth
+// Role-based authorization middleware
+const authorizeRoles = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return res.status(403).json({ message: 'Access denied' });
+    }
+    next();
+  };
+};
+
+module.exports = { auth, authorizeRoles };
--- routes/AuthRouter.js
+++ routes/AuthRouter.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const { register, login, getAllUsers, resetPassword, forgotPassword } = require('../controller/auth')
+const { auth, authorizeRoles } = require('../middleware/authentication')
 
-router.get('/', getAllUsers)
+router.get('/',auth,authorizeRoles('admin'), getAllUsers)
 router.post('/register', register)
 router.post('/login', login)
 router.post('/forgotPassword', forgotPassword)
